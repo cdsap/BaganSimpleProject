02c226e3a1efaaf8bdc4adda4c2d13a0fa3d4a5d
Inaki Villar <inaki.seri@gmail.com>
2019-06-21 18:48:42 +0700

    initial import

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+buildscript {
+    repositories {
+        google()
+        jcenter()
+
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:3.4.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.21")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+
+    }
+}
